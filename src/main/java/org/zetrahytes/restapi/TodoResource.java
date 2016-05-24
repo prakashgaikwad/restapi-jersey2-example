cb70b558aa2c4b10f2b960182f5213a2f598d30e
Subramanian, Singaram <singsubramanian@magento.com>
2016-05-24 14:01:42 -0700

    swagger added to produce Swagger 2.0 definition files; generated swagger spec available at http://localhost:8000/myapp/swagger.json and http://localhost:8000/myapp/swagger.yaml with jetty up and running

--- src/main/java/org/zetrahytes/restapi/TodoResource.java
+++ src/main/java/org/zetrahytes/restapi/TodoResource.java
@@ -9,6 +9,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -20,53 +21,69 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.zetrahytes.restapi.entity.Todo;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 import static java.util.Objects.isNull;
 
 @Path("todos")
+@Api(value = "/todos")
 public class TodoResource {
-    
+
     private static final Logger LOG = LoggerFactory.getLogger(TodoResource.class);
-	private static Map<Long, Todo> todos;
+    private static Map<Long, Todo> todos;
 
-	static {
-		todos = new ConcurrentHashMap<>(); // thread-safe
-		Date today = new Date();
-		todos.put(1L, new Todo(1, "add a working jersey2 example", true, today));
-		todos.put(2L, new Todo(2, "add checkstyle", false, today));
-		LOG.info("Populated some todos");
-	}
+    static {
+        todos = new ConcurrentHashMap<>(); // thread-safe
+        Date today = new Date();
+        todos.put(1L, new Todo(1, "add a working jersey2 example", true, today));
+        todos.put(2L, new Todo(2, "add checkstyle", false, today));
+        LOG.info("Populated some todos");
+    }
 
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	public Collection<Todo> list() {
-		return todos.values();
-	}
-	
-	@GET
-	@Path("{id}")
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response todoById(@PathParam("id") long id) {
-		Todo todo = todos.get(id);
-		if (isNull(todo)) {
-			return Response.status(Status.NOT_FOUND).build();
-		}
-		return Response.ok(todo).build();
-	}
-	
-	@POST
-	@Consumes(MediaType.APPLICATION_JSON)
-	public Response addTodo(Todo todo) {
-		todos.put(todo.getId(), todo);
-		return Response.noContent().build(); //204 - Todo added
-	}
-	
-	@DELETE
-	@Path("{id}")
-	public Response remove(@PathParam("id") long id) {
-		Todo removedTodo = todos.remove(id);
-		if (isNull(removedTodo)) {
-			return Response.status(Status.NOT_FOUND).build();
-		}
-		return Response.noContent().build(); //204 - Todo removed
-	}
+    @GET
+    @ApiOperation(value = "Returns all todos")
+    @ApiResponses(value = { @ApiResponse(code = 200, message = "returns a list of todos in json format") })
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAllTodos() {
+        Collection<Todo> todoCollection = todos.values();
+        return Response.ok(todoCollection).build();
+    }
+
+    @GET
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getTodoById(@PathParam("id") long id) {
+        Todo todo = todos.get(id);
+        if (isNull(todo)) {
+            return Response.status(Status.NOT_FOUND).build();
+        }
+        return Response.ok(todo).build();
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response addTodo(Todo todo) {
+        todos.put(todo.getId(), todo);
+        return Response.noContent().build(); // 204 - Todo added
+    }
+
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response updateTodo(Todo todo) {
+        todos.put(todo.getId(), todo);
+        return Response.noContent().build(); // 204 - Todo added
+    }
+
+    @DELETE
+    @Path("{id}")
+    public Response remove(@PathParam("id") long id) {
+        Todo removedTodo = todos.remove(id);
+        if (isNull(removedTodo)) {
+            return Response.status(Status.NOT_FOUND).build();
+        }
+        return Response.noContent().build(); // 204 - Todo removed
+    }
 }
